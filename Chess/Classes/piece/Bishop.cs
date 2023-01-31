ee4dd0100ab8e4b6747c0163211d11385ede9cd0
Joe Lenton <jlenton@me.com>
2023-01-31 16:30:41 +0000

    adds rest of piece classes

---
+++ Chess/Classes/piece/Bishop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.Classes.common;
+using Chess.Classes.squares;
+using Chess.Classes.piece;
+using File = Chess.Classes.common.File;
+
+namespace Chess.Classes.piece
+{
+    public class Bishop : Piece.Piece
+    {
+        public Bishop(PieceColour pieceColour, Location location, bool isAlive)
+        {
+            this.pieceColour = pieceColour;
+            this.pieceType = PieceType.BISHOP;
+            this.location = location;
+            this.isAlive = false; 
+        }
+    }
+}
---
+++ Chess/Classes/piece/King.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.Classes.common;
+using Chess.Classes.squares;
+using Chess.Classes.piece;
+using File = Chess.Classes.common.File;
+
+namespace Chess.Classes.piece
+{
+    public class King : Piece.Piece
+    {
+        public King(PieceColour pieceColour, Location location, bool isAlive)
+        {
+            this.pieceColour = pieceColour;
+            this.pieceType = PieceType.KING;
+            this.location = location;
+            this.isAlive = false;
+        }
+    }
+}
